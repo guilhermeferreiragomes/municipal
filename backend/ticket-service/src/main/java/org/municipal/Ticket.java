5f1bafb11c3d09f06b1ab337ba46dcaf952a0447
Guilherme Ferreira Gomes <gffgs@iscte-iul.pt>
2026-05-19 23:46:39 +0200

    Backend: Criado o ticket-service com modelo de dados e rotas REST

---
+++ backend/ticket-service/src/main/java/org/municipal/Ticket.java
@@ -0,0 +1,27 @@
+package org.municipal;
+
+import io.quarkus.mongodb.panache.PanacheMongoEntity;
+import java.time.LocalDateTime;
+
+public class Ticket extends PanacheMongoEntity {
+
+    public enum TicketStatus {
+        OPEN,
+        IN_PROGRESS,
+        RESOLVED,
+        CLOSED,
+        REJECTED
+    }
+
+    public String title;
+    public String description;
+    public TicketStatus status;
+    public String imageUrl;
+    public LocalDateTime createdAt;
+
+    // Construtor
+    public Ticket() {
+        this.createdAt = LocalDateTime.now();
+        this.status = TicketStatus.OPEN;
+    }
+}
---
+++ backend/ticket-service/src/main/java/org/municipal/TicketResource.java
@@ -0,0 +1,46 @@
+package org.municipal;
+
+import java.util.List;
+
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import org.bson.types.ObjectId;
+
+@Path("/tickets") // URL for the tickets "localhost:8080/tickets"
+@Produces(MediaType.APPLICATION_JSON) // Answer with Json
+@Consumes(MediaType.APPLICATION_JSON) // Receive a Json
+
+public class TicketResource {
+
+    // Read the list of all tickets
+    @GET
+    public List<Ticket> getAllTickets() {
+        return Ticket.listAll();
+    }
+
+    // Create a new ticket
+    @POST
+    public Response createTicket(Ticket t) {
+        t.persist(); // To save into MongoDB
+        return Response.status(Response.Status.CREATED).entity(t).build(); // Returns "success" with the created ticket
+        // converted to JSON
+    }
+
+    @PUT
+    @Path("/{id}/status")
+    public Response updateTicketStatus(@PathParam("id") String id, Ticket ticketUpdate) { // Mongo creats werid IDs
+
+        Ticket ticket = Ticket.findById(new ObjectId(id));
+        if (ticket == null) {
+            return Response.status(Response.Status.NOT_FOUND).entity("Ticket not found.").build();
+        }
+
+        ticket.status = ticketUpdate.status; // Updates the status of the tickets
+
+        ticket.update(); // Saves in MongoDB
+
+        return Response.ok(ticket).build();
+    }
+
+}
